b3dd985c67d6055b82894bf6b2cd3c5360a1d411
Ищанов Бахтияр Есенович <ischanov.be1608@okeit.edu>
2023-04-24 14:01:26 +0500

    Добавьте файлы проекта.

---
+++ Calculationlib/CalculationLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculationLib
+{
+     static public class Calculation
+    {
+        static public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        static public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        static public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        static public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
+
---
+++ Calculationlib/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CalculationLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double a, b, result;
+
+            Console.Write("Введите первое число: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            b = double.Parse(Console.ReadLine());
+
+            result = Calculation.Plus(a, b);
+            Console.WriteLine($"{a} + {b} = {result}");
+
+            result = Calculation.Minus(a, b);
+            Console.WriteLine($"{a} - {b} = {result}");
+
+            result = Calculation.Divide(a, b);
+            Console.WriteLine($"{a} / {b} = {result}");
+
+            result = Calculation.Multiply(a, b);
+            Console.WriteLine($"{a} * {b} = {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
